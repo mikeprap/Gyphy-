fd1a3d12986d91c37854951a66bfa90001a60fe5
Mike Prap <michaelpraplaski@yahoo.com>
2019-09-16 16:44:58 -0400

    added an on click funtion and a for loop

--- assets/java/game.js
+++ assets/java/game.js
@@ -3,9 +3,32 @@ var topics = ["football", "baseball", "soccer", "hockey"]
 var queryUrl = "https://api.giphy.com/v1/gifs/trending?api_key=e8VuvuWDCwArUBIdlcHCID93dVh58Qnz";
 
 
+
+$("button").on("click", function(){
+    var sports = $(this).attr("data-sports");
+
+    var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + sports + "&api_key=e8VuvuWDCwArUBIdlcHCID93dVh58Qnz&limit=10";
+
+
+
+
+
+
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
+    console.log(queryUrl);
+    
     console.log(response);
+
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++){
+
+        var sportsDiv = $("<div>");
+
+    }
   });
+
+});
